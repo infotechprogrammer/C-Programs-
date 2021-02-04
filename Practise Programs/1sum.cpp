14d3b82d4689ac7c690b70b6d5d85fcd5e438ad5
infotechprogramming <78542194+infotechprogramming@users.noreply.github.com>
2021-02-04 18:21:44 +0530

    Add files via upload

---
+++ Practise Programs/1sum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{ 
+    int a,b,sum; //variables
+    cout<<"Enter Number a =\t"; //\t for space
+    cin>>a;                                                
+    cout<<"Enter Number b =\t";
+    cin>>b;
+    sum = a+b; // + operator is used
+    cout<<"Sum = "<<sum;
+    return 0;
+
+}
---
+++ Practise Programs/2sizeofoperator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    float f;
+    cout<<"Size of int = "<<sizeof(i);
+    cout<<"\nSize of int = "<<sizeof(int);
+    cout<<"\nSize of float = "<<sizeof(f);
+    cout<<"\nSize of float = "<<sizeof(float);
+    cout<<"\nSize of char = "<<sizeof(char);
+
+return 0;
+}
